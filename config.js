1243d41ba55be0ba996d764a41317eef60adbbf1
socialvibestudiopk-source <socialvibestudiopk@gmail.com>
2026-08-10 23:01:12 +0500

    Deployment preparation

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  //port
+  PORT: process.env.PORT || 5000,
+
+  //secret key for API
+  SECRET_KEY: "Lumora_Audio_2023_Secure_Key",
+
+  //gmail credentials for send email
+  EMAIL: "YOUR_EMAIL_HERE",
+  PASSWORD: "YOUR_APP_PASSWORD_HERE",
+
+  //secret key for jwt
+  JWT_SECRET: "Lumora_JWT_Token_Secret_2023",
+
+  SERVER_PATH: "http://localhost:5000",
+
+  // License Key / Purchase Code
+  PURCHASE_CODE: "2026Rayzi",
+
+  // firebase server key for notification
+  SERVER_KEY: "YOUR_FIREBASE_SERVER_KEY_HERE",
+
+  // Mongodb Connection String
+  MONGODB_CONNECTION_STRING: "mongodb+srv://novaprime953_db_user:j1L3zirhl8TnKdY1@cluster0.icfhait.mongodb.net/?appName=Cluster0"
+};
+
